dbdca89d4ed8e9798026fc2245c96bb2f08ea8cd
s6s6s6 <41319794+s6s6s6@users.noreply.github.com>
2018-07-30 16:56:13 +0800

    7.30

--- XiaoMiShop/src/com/ssm/model/bean/Goods.java
+++ XiaoMiShop/src/com/ssm/model/bean/Goods.java
@@ -10,65 +10,45 @@ public class Goods {
 	private String goods_pic_url;
 	private String goods_desc_pic_url;
 	private String video_set_url;
-<<<<<<< HEAD
 	private List<GoodsAttribute> attributeList;
 	private List<GoodsDetail> detailList;
 	private List<GoodsPreviewPicture> previewPictureList;
-=======
-	private List<GoodsAttribute> goodsAttributeList;
-	private List<GoodsDetail> goodsDetailList;
-	private List<GoodsPreviewPicture> goodsPreviewPictureList;
-
->>>>>>> master
 	public int getGoods_id() {
 		return goods_id;
 	}
-
 	public void setGoods_id(int goods_id) {
 		this.goods_id = goods_id;
 	}
-
 	public int getGoods_category_id() {
 		return goods_category_id;
 	}
-
 	public void setGoods_category_id(int goods_category_id) {
 		this.goods_category_id = goods_category_id;
 	}
-
 	public int getGoods_code() {
 		return goods_code;
 	}
-
 	public void setGoods_code(int goods_code) {
 		this.goods_code = goods_code;
 	}
-
 	public String getGoods_name() {
 		return goods_name;
 	}
-
 	public void setGoods_name(String goods_name) {
 		this.goods_name = goods_name;
 	}
-<<<<<<< HEAD
 	public String getGoods_pic_url() {
 		return goods_pic_url;
 	}
 	public void setGoods_pic_url(String goods_pic_url) {
 		this.goods_pic_url = goods_pic_url;
 	}
-=======
-
->>>>>>> master
 	public String getVideo_set_url() {
 		return video_set_url;
 	}
-
 	public void setVideo_set_url(String video_set_url) {
 		this.video_set_url = video_set_url;
 	}
-<<<<<<< HEAD
 	public List<GoodsAttribute> getAttributeList() {
 		return attributeList;
 	}
@@ -87,41 +67,6 @@ public class Goods {
 	public void setPreviewPictureList(List<GoodsPreviewPicture> previewPictureList) {
 		this.previewPictureList = previewPictureList;
 	}
-=======
-
-	public List<GoodsAttribute> getGoodsAttributeList() {
-		return goodsAttributeList;
-	}
-
-	public void setGoodsAttributeList(List<GoodsAttribute> goodsAttributeList) {
-		this.goodsAttributeList = goodsAttributeList;
-	}
-
-	public List<GoodsDetail> getGoodsDetailList() {
-		return goodsDetailList;
-	}
-
-	public void setGoodsDetailList(List<GoodsDetail> goodsDetailList) {
-		this.goodsDetailList = goodsDetailList;
-	}
-
-	public List<GoodsPreviewPicture> getGoodsPreviewPictureList() {
-		return goodsPreviewPictureList;
-	}
-
-	public void setGoodsPreviewPictureList(List<GoodsPreviewPicture> goodsPreviewPictureList) {
-		this.goodsPreviewPictureList = goodsPreviewPictureList;
-	}
-
-	public String getGoods_pic_url() {
-		return goods_pic_url;
-	}
-
-	public void setGoods_pic_url(String goods_pic_url) {
-		this.goods_pic_url = goods_pic_url;
-	}
-
->>>>>>> master
 	public String getGoods_desc_pic_url() {
 		return goods_desc_pic_url;
 	}
